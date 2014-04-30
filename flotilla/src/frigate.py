5cfe5708b37ba5b3df9c7c08f43b6950f1ef6294
Michael Lovci <michaeltlovci@gmail.com>
2014-04-30 13:44:53 -0700

    added NMF, made general Pretty_Reducer class

--- flotilla/src/frigate.py
+++ flotilla/src/frigate.py
@@ -282,53 +282,13 @@ def dropna_mean(x):
 
 
 import sklearn
+from sklearn import decomposition
 
 
-class PCA(sklearn.decomposition.PCA):
+class Pretty_Reducer(object):
     """
 
-    Just like sklearn's PCA, but with prettied up DataFrames.
-
-    """
-
-    def relabel_pcs(self, x):
-        return "pc_" + str(int(x) + 1)
-
-    def fit(self, X):
-
-        try:
-            assert type(X) == pd.DataFrame
-        except:
-            print "Try again as a pandas data frame"
-            raise
-
-        self.X = X
-        super(PCA, self).fit(X)
-        self.components_ = pd.DataFrame(self.components_, columns=self.X.columns).rename_axis(self.relabel_pcs, 0)
-        self.explained_variance_ = pd.Series(self.explained_variance_).rename_axis(self.relabel_pcs, 0)
-        self.explained_variance_ratio_ = pd.Series(self.explained_variance_ratio_).rename_axis(self.relabel_pcs, 0)
-        return self
-
-    def transform(self, X):
-        pca_space = super(PCA, self).transform(X)
-        if type(self.X) == pd.DataFrame:
-            pca_space = pd.DataFrame(pca_space, index=self.X.index).rename_axis(self.relabel_pcs, 1)
-        return pca_space
-
-    def fit_transform(self, X):
-        try:
-            assert type(X) == pd.DataFrame
-        except:
-            print "Try again as a pandas data frame"
-            raise
-        self.fit(X)
-        return self.transform(X)
-
-
-class NMF(sklearn.decomposition.NMF):
-    """
-
-    Just like sklearn's PCA, but with prettied up DataFrames.
+    Just like sklearn's reducers, but with prettied up DataFrames.
 
     """
 
@@ -344,15 +304,20 @@ class NMF(sklearn.decomposition.NMF):
             raise
 
         self.X = X
-        super(NMF, self).fit(X)
+        super(Pretty_Reducer, self).fit(X)
         self.components_ = pd.DataFrame(self.components_, columns=self.X.columns).rename_axis(self.relabel_pcs, 0)
+        try:
+            self.explained_variance_ = pd.Series(self.explained_variance_).rename_axis(self.relabel_pcs, 0)
+            self.explained_variance_ratio_ = pd.Series(self.explained_variance_ratio_).rename_axis(self.relabel_pcs, 0)
+        except AttributeError:
+            pass
         return self
 
     def transform(self, X):
-        component_space = super(NMF, self).transform(X)
+        component_space = super(Pretty_Reducer, self).transform(X)
         if type(self.X) == pd.DataFrame:
-                component_space = pd.DataFrame(    component_space, index=self.X.index).rename_axis(self.relabel_pcs, 1)
-        return     component_space
+                component_space = pd.DataFrame(component_space, index=self.X.index).rename_axis(self.relabel_pcs, 1)
+        return component_space
 
     def fit_transform(self, X):
         try:
@@ -362,3 +327,30 @@ class NMF(sklearn.decomposition.NMF):
             raise
         self.fit(X)
         return self.transform(X)
+
+class PCA(Pretty_Reducer, sklearn.decomposition.PCA):
+    pass
+
+class NMF(Pretty_Reducer, sklearn.decomposition.NMF):
+    here=False
+    def fit(self, X):
+
+        """
+        duplicated fit code for NMF because sklearn's NMF cheats for efficiency and calls fit_transform.
+        MRO resolves the closest (in this package) fit_transform first and so there's a recursion error:
+            def fit(self, X, y=None, **params):
+
+                self.fit_transform(X, **params)
+                return self
+        """
+
+        try:
+            assert type(X) == pd.DataFrame
+        except:
+            print "Try again as a pandas data frame"
+            raise
+
+        self.X = X
+        super(sklearn.decomposition.NMF, self).fit_transform(X)  #notice this is fit_transform, not fit
+        self.components_ = pd.DataFrame(self.components_, columns=self.X.columns).rename_axis(self.relabel_pcs, 0)
+
