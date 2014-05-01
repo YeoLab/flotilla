ac1f8d4fa86871f2071fc52d8ab104b098f646c1
Michael Lovci <michaeltlovci@gmail.com>
2014-04-30 17:47:37 -0700

    adding olga's fxns

--- flotilla/src/submarine.py
+++ flotilla/src/submarine.py
@@ -322,3 +322,66 @@ def plot_pca(df, **kwargs):
     return_me, ax = pcaObj.plot_samples()
     return return_me
 
+
+def lavalamp(psi, color=None, title='', ax=None):
+    from .frigate import get_switchy_score_order
+    """Make a 'lavalamp' scatter plot of many spliciang events
+
+    Useful for visualizing many splicing events at once.
+
+    Parameters
+    ----------
+    df : array
+        A (n_events, n_samples) matrix either as a numpy array or as a pandas
+        DataFrame
+
+    color : matplotlib color
+        Color of the scatterplot. Defaults to a dark teal
+
+    title : str
+        Title of the plot. Default ''
+
+    ax : matplotlib.Axes object
+        The axes to plot on. If not provided, will be created
+
+
+    Returns
+    -------
+    fig : matplotlib.Figure
+        A figure object for saving.
+    """
+    import matplotlib.pyplot as plt
+
+    if ax is None:
+        fig, ax = plt.subplots(figsize=(16,4))
+    else:
+        fig = plt.gcf()
+    nrow, ncol = psi.shape
+    x = np.vstack(np.arange(nrow) for _ in range(ncol))
+
+    color = '#FFFFFF' if color is None else color
+
+    try:
+        # This is a pandas Dataframe
+        y = psi.values.T
+    except AttributeError:
+        # This is a numpy array
+        y = psi.T
+
+    order = get_switchy_score_order(y)
+    y = y[:, order]
+
+    # Add one so the last value is actually included instead of cut off
+    xmax = x.max() + 1
+    ax.scatter(x, y, color=color, alpha=0.5, edgecolor='#262626', linewidth=0.1)
+    seaborn.despine()
+    ax.set_ylabel('$\Psi$')
+    ax.set_xlabel('{} splicing events'.format(nrow))
+    ax.set_xticks([])
+
+    ax.set_xlim(0, xmax)
+    ax.set_ylim(0, 1)
+    ax.set_title(title)
+
+    # Return the figure for saving
+    # return fig
