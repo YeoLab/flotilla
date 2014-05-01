30dfb86e502f3c021ba4acbb51ba2a711d86da31
Michael Lovci <michaeltlovci@gmail.com>
2014-04-30 17:47:37 -0700

    adding olga's fxns

--- flotilla/src/frigate.py
+++ flotilla/src/frigate.py
@@ -1,5 +1,5 @@
 from __future__ import division
-__author__ = 'lovci'
+__author__ = 'lovci, obot'
 
 
 """
@@ -21,6 +21,47 @@ import pandas as pd
 from sklearn.ensemble import ExtraTreesRegressor, GradientBoostingRegressor
 from scipy import stats
 from barge import timeout, TimeoutError
+
+def switchy_score(array):
+    """Transform a 1D array of df scores to a vector of "switchy scores"
+
+    Calculates std deviation and mean of sine- and cosine-transformed
+    versions of the array. Better than sorting by just the mean which doesn't
+    push the really lowly variant events to the ends.
+
+    Parameters
+    ----------
+    array : numpy.array
+        A 1-D numpy array or something that could be cast as such (like a list)
+
+    Returns
+    -------
+    float
+        The "switchy score" of the data which can then be compared to other
+        splicing event data
+
+    @author Michael T. Lovci
+    """
+    array = np.array(array)
+    variance = 1 - np.std(np.sin(array[~np.isnan(array)] * np.pi))
+    mean_value = -np.mean(np.cos(array[~np.isnan(array)] * np.pi))
+    return variance * mean_value
+
+def get_switchy_score_order(x):
+    """Apply switchy scores to a 2D array of df scores
+
+    Parameters
+    ----------
+    x : numpy.array
+        A 2-D numpy array in the shape [n_events, n_samples]
+
+    Returns
+    -------
+    numpy.array
+        A 1-D array of the ordered indices, in switchy score order
+    """
+    switchy_scores = np.apply_along_axis(switchy_score, axis=0, arr=x)
+    return np.argsort(switchy_scores)
 
 
 def binify(df, binsize, vmin=0, vmax=1):
