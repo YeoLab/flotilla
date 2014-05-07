9130408bc94e24d8e95d5c749ebfbc6609e639b5
Michael Lovci <michaeltlovci@gmail.com>
2014-05-07 12:23:26 -0700

    added barebones project

---
+++ barbones_project/barbones_project/__init__.py
@@ -0,0 +1,5 @@
+__author__ = 'lovci'
+
+
+from params import *
+from study_data import *
---
+++ barbones_project/barbones_project/src/utils.py
@@ -0,0 +1,7 @@
+__author__ = 'lovci'
+import os
+
+def data_dir():
+
+    return os.path.abspath(os.path.join(os.path.dirname(__file__), '../study_data'))
+
