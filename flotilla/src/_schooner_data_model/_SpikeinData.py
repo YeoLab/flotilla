d04e6352196f92ce18052c0fd052253dfcdbeba9
Olga Botvinnik <olga.botvinnik@gmail.com>
2014-06-09 16:24:51 -0700

    Initial SpikeInData

---
+++ flotilla/src/_schooner_data_model/_SpikeinData.py
@@ -0,0 +1,29 @@
+__author__ = 'olga'
+
+class SpikeInData(ExpressionData):
+    """Class for Spikein data and associated functions
+    Attributes
+    ----------
+
+
+    Methods
+    -------
+
+    """
+
+    def __init__(self, df, sample_metadata):
+        """Constructor for
+
+        Parameters
+        ----------
+        df, sample_metadata
+
+        Returns
+        -------
+
+
+        Raises
+        ------
+
+        """
+        pass
