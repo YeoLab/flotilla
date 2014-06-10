0558de6bf399fe0f98d304e947d12381b26f100a
Michael Lovci <michaeltlovci@gmail.com>
2014-06-10 00:11:12 -0700

    barebones_project now a submodule of the barebones branch of flotilla_project

--- barebones_project/barebones_project/study_data/__init__.py
+++
@@ -1,8 +0,0 @@
-__author__ = 'lovci'
-
-import os, sys
-study_data_dir = os.path.dirname(__file__)
-with open(os.path.join(study_data_dir, "README"), 'r') as f:
-    for line in f.readlines():
-        if not line.startswith("#"):
-            sys.stderr.write(line)
