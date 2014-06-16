d64b9126adb90a817546736b6e0933b17a70c236
Olga Botvinnik <olga.botvinnik@gmail.com>
2014-06-16 13:24:56 -0700

    fix function import and connects, add docstring and naming conventions
    Former-commit-id: 9cd4811efd05287860c32a8825642cdcaeb24a06

---
+++ flotilla/util.py
@@ -0,0 +1,72 @@
+__author__ = 'lovci'
+
+"""
+
+general utilities
+
+"""
+
+from functools import wraps
+import errno
+import os
+import signal
+import sys
+import subprocess
+import pandas as pd
+
+###http://stackoverflow.com/questions/2281850/timeout-function-if-it-takes-too-long-to-finish###
+
+class TimeoutError(Exception):
+    pass
+
+
+def timeout(seconds=10, error_message=os.strerror(errno.ETIME)):
+    def decorator(func):
+        def _handle_timeout(signum, frame):
+            raise TimeoutError(error_message)
+
+        def wrapper(*args, **kwargs):
+            signal.signal(signal.SIGALRM, _handle_timeout)
+            signal.alarm(seconds)
+            try:
+                result = func(*args, **kwargs)
+            finally:
+                signal.alarm(0)
+            return result
+
+        return wraps(func)(wrapper)
+
+    return decorator
+###http://stackoverflow.com/questions/2281850/timeout-function-if-it-takes-too-long-to-finish###
+
+
+def serve_ipython():
+    try:
+
+        assert len(sys.argv) == 2
+        path = sys.argv[1]
+        assert os.path.exists(sys.argv[1])
+
+    except:
+        raise ValueError("specify a notebook directory as the first and only argument")
+
+    c = subprocess.Popen(['ipython', 'notebook', '--script', '--notebook-dir', path])
+    try:
+        c.wait()
+    except KeyboardInterrupt:
+        c.terminate()
+
+
+def dict_to_str(dic):
+        """join dictionary study_data into a string with that study_data"""
+        return "_".join([k + ":" + str(v) for (k, v) in dic.items()])
+
+def install_development_package(package_location):
+    original_location = os.getcwd()
+    os.chdir(package_location)
+    subprocess.call(['pip install -e %s' % package_location], shell=True)
+    os.chdir(original_location)
+
+#def path_to_this_file():
+#
+#    return os.path.join(os.path.dirname(__file__))
---
+++ flotilla/visualize/__init__.py
@@ -0,0 +1,50 @@
+__author__ = 'lovci, ppliu, obot, ....?'
+
+""""plotting tools"""
+
+
+__author__ = 'olga'
+
+import math
+from math import sqrt
+import matplotlib.pyplot as plt
+
+
+import numpy as np
+from numpy.linalg import norm
+import pandas as pd
+import seaborn
+# from ..neural_diff_project.project_params import _default_group_id
+
+seaborn.set_style({'axes.axisbelow': True,
+                   'axes.edgecolor': '.15',
+                   'axes.facecolor': 'white',
+                   'axes.grid': False,
+                   'axes.labelcolor': '.15',
+                   'axes.linewidth': 1.25,
+                   'font.family': 'Helvetica',
+                   'grid.color': '.8',
+                   'grid.linestyle': '-',
+                   'image.cmap': 'Greys',
+                   'legend.frameon': False,
+                   'legend.numpoints': 1,
+                   'legend.scatterpoints': 1,
+                   'lines.solid_capstyle': 'round',
+                   'text.color': '.15',
+                   'xtick.color': '.15',
+                   'xtick.direction': 'out',
+                   'xtick.major.size': 0,
+                   'xtick.minor.size': 0,
+                   'ytick.color': '.15',
+                   'ytick.direction': 'out',
+                   'ytick.major.size': 0,
+                   'ytick.minor.size': 0})
+
+seaborn.set_palette('deep')
+blue = seaborn.color_palette()[0]
+green =seaborn.color_palette()[1]
+red = seaborn.color_palette()[2]
+
+from network import Networker, NetworkerViz
+from predictor import PredictorViz
+from ipython_interact import Interactive
