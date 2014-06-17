3e021fba1fce5a6ca5187c473b088c690292c3dd
Olga Botvinnik <olga.botvinnik@gmail.com>
2014-06-16 17:59:07 -0700

    lesson learned: don't try to subclass pandas.DataFrame
    Former-commit-id: 961069dbc2413be7f365f410484629c25afe04d4

--- flotilla/test/test_basedata.py
+++ flotilla/test/test_basedata.py
@@ -1,8 +1,9 @@
 __author__ = 'olga'
 
 from ..data_model._BaseData import BaseData
+import pandas.util.testing as pdt
 
 def test_init(example_data):
     base_data = BaseData(example_data.metadata, example_data.expression)
-    assert base_data.metadata == example_data.metadata
-    assert base_data._data == example_data.expression
+    pdt.assert_frame_equal(base_data.sample_metadata, example_data.metadata)
+    pdt.assert_frame_equal(base_data, example_data.expression)
