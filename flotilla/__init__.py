c4c12f26edea3101f8a3845a277b047d70eca292
Olga Botvinnik <olga.botvinnik@gmail.com>
2014-06-19 14:51:43 -0700

    move embark to __init__
    Former-commit-id: b14e2bb652b2fd2eb5bd0b2bf1e58aa1506b7951

--- flotilla/__init__.py
+++ flotilla/__init__.py
@@ -1,6 +1,11 @@
 __author__ = 'lovci'
 
-from embark import Embark
+import urllib2
+
+import simplejson
+
+from .data_model.study import StudyFactory, Study
+
 
 # import computation as frigate
 # import database as carrier
@@ -20,5 +25,40 @@ def main():
     pass
 
 
-def embark(datapackge_url):
-    return Embark(datapackge_url)
+class Embark(object):
+    """
+    Begin your journey of data exploration
+    """
+
+    def __init__(self, data_package_url):
+        self.data_package_url = data_package_url
+        self.data_package = self.fetch_data_package()
+
+    def fetch_data_package(self):
+        req = urllib2.Request(self.data_package_url)
+        opener = urllib2.build_opener()
+        f = opener.open(req)
+        return simplejson.load(f)
+
+    def create_package(self):
+        """
+
+        """
+        # Seems contrived that we're instantiating StudyFactory and just
+        # using one method from it..
+        study = StudyFactory()
+
+        dfs = {}
+        for resource in self.data_package['resources']:
+            resource_url = resource['url']
+
+            dfs[resource['name']] = study._load_tsv(resource_url)
+
+        return Study(experiment_design_data=dfs['experiment_design'],
+                     expression_data=dfs['expression'],
+                     splicing_data=dfs['splicing'])
+
+
+def embark(data_package_url):
+    study = Embark(data_package_url)
+    return study.create_package()
