8937cea571c46d6c1abce034f44b549c6c56adba
Olga Botvinnik <olga.botvinnik@gmail.com>
2014-06-20 09:21:28 -0700

    Remove Embark object from flotilla/__init__.py

--- flotilla/__init__.py
+++ flotilla/__init__.py
@@ -1,8 +1,5 @@
 __author__ = 'lovci'
 
-import urllib2
-import json
-
 from .data_model.study import StudyFactory, Study
 
 
@@ -17,38 +14,10 @@ def main():
     pass
 
 
-class Embark(object):
-    """
-    Begin your journey of data exploration
-    """
-    def __init__(self, data_package_url):
-        self.data_package_url = data_package_url
-        self.data_package = self.fetch_data_package()
-
-    def fetch_data_package(self):
-        req = urllib2.Request(self.data_package_url)
-        opener = urllib2.build_opener()
-        f = opener.open(req)
-        return json.load(f)
-
-    def create_package(self):
-        """
-
-        """
-        # Seems contrived that we're instantiating StudyFactory and just
-        # using one method from it..
-        study = StudyFactory()
-
-        dfs = {}
-        for resource in self.data_package['resources']:
-            resource_url = resource['url']
-
-            dfs[resource['name']] = study._load_tsv(resource_url)
-
-        return Study(experiment_design_data=dfs['experiment_design'],
-                     expression_data=dfs['expression'],
-                     splicing_data=dfs['splicing'])
 
 
 def embark(data_package_url):
+    """
+    Begin your journey of data exploration
+    """
     return Study.from_data_package_url(data_package_url)
