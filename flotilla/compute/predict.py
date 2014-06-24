1df9d948ff04715db1b2fbe948e111c7252bec93
Olga Botvinnik <olga.botvinnik@gmail.com>
2014-06-24 16:02:39 -0700

    Move score() to PredictorBase
    Former-commit-id: fe7c4a72196e419abb52d48daa886fccc42f036b

--- flotilla/compute/predict.py
+++ flotilla/compute/predict.py
@@ -22,6 +22,41 @@ class PredictorBase(object):
     def __init__(self, data, trait_data, predictor=None, name="Predictor",
                  predictor_kwargs=None, predictor_scoring_fun=None,
                  score_cutoff_fun=None):
+        """Initializer for scikit-learn predictors (classifiers and regressors)
+
+        Initalizes everything except the "y" (response variable). This must
+        be initialized by the base class.
+
+        Parameters
+        ----------
+        data : pandas.DataFrame
+            Samples x features data to train the predictor on
+        trait_data : pandas.Series
+            Samples x trait (single) data that you want to tell the
+            difference between
+        predictor : scikit-learn classifier
+            Which regression to use. Default ExtraTreesClassifier
+        name : str
+            Titles for plots and things... Default "ExtraTreesRegressor"
+        predictor_kwargs : dict
+            Extra arguments to the predictor. Default:
+            {'n_estimators': 100,
+             'bootstrap': True,
+             'max_features': 'auto',
+             'random_state': 0,
+             'oob_score': True,
+             'n_jobs': 2,
+             'verbose': True}
+        predictor_scoring_fun : function
+            Function to get the feature scores for a scikit-learn classifier.
+            This can be different for different classifiers, e.g. for a
+            classifier named "clf" it could be clf.scores_, for other it's
+            clf.feature_importances_. Default: lambda clf: clf.feature_importances_
+        score_cutoff_fun : function
+            Function to cut off insignificant scores (scores as returned by
+            predictor_scoring_fun)
+            Default: lambda scores: np.mean(scores) + 2 * np.std(scores)
+        """
         self.predictor_class = predictor
 
         self.has_been_fit_yet = False
@@ -54,13 +89,32 @@ class PredictorBase(object):
         self.predictor = None
 
     def fit(self):
-        """ fit classifiers to the data
+        """Fit predictor to the data
         """
         self.predictor = self.predictor_class(**self.predictor_kwargs)
         sys.stdout.write("Fitting a predictor for trait {}... please wait.\n"
                          .format(self.trait))
         self.predictor.fit(self.X, self.y)
         sys.stdout.write("\tFinished.\n")
+
+    def score(self):
+        """Collect scores from predictor
+        """
+        sys.stdout.write("Scoring predictor: {} for trait: {}... please "
+                         "wait\n".format(self.name, self.trait))
+        self.predictor.scores_ = pd.Series(index=self.X.columns,
+                                           data=self.predictor_scoring_fun(
+                                               self.predictor))
+        self.predictor.score_cutoff_ = \
+            self.score_cutoff_fun(self.predictor.scores_)
+        self.predictor.good_features_ = self.predictor.scores_ > self.predictor.score_cutoff_
+        self.important_features = self.predictor.good_features_
+        self.predictor.n_good_features_ = np.sum(
+            self.predictor.good_features_)
+        self.predictor.subset_ = self.X.T[self.predictor.good_features_].T
+
+        sys.stdout.write("\tFinished.\n")
+        self.has_been_scored_yet = True
 
 
 class Regressor(PredictorBase):
@@ -76,6 +130,8 @@ class Regressor(PredictorBase):
                  score_cutoff_fun=None):
         """Train a regressor on data.
 
+        Parameters
+        ----------
         data : pandas.DataFrame
             Samples x features data to train the predictor on
         trait_data : pandas.Series
@@ -108,59 +164,6 @@ class Regressor(PredictorBase):
         self.y = self.trait_data
 
 
-    def score(self,
-              traits=None,
-              regressor_name=default_regressor_name,
-              feature_scoring_fun=default_regressor_scoring_fun,
-              score_cutoff_fun=default_regressor_scoring_cutoff_fun):
-        """
-        collect scores from classifiers_
-        feature_scoring_fun: fxn that yields higher values for better features
-        score_cutoff_fun fxn that that takes output of feature_scoring_fun and returns a cutoff
-        """
-        raise NotImplementedError("Untested, should be close to working.")
-        if traits is None:
-            traits = self.continuous_traits
-
-        for trait in traits:
-
-            try:
-                assert trait in self.regressors_
-            except:
-                print "trait: %s" % trait, "is missing, continuing"
-                continue
-            try:
-                assert regressor_name in self.regressors_[trait]
-            except:
-                print "predictor: %s" % regressor_name, "is missing, continuing"
-                continue
-
-            print "Scoring predictor: %s for trait: %s... please wait." % (
-                regressor_name, trait)
-
-            clf = self.regressors_[trait][regressor_name]
-            clf.scores_ = pd.Series(feature_scoring_fun(clf),
-                                    index=self.X.columns)
-            clf.score_cutoff_ = score_cutoff_fun(clf.scores_)
-            self.important_features[trait][regressor_name] = clf.good_features_
-            clf.good_features_ = clf.scores_ > clf.score_cutoff_
-            clf.n_good_features_ = np.sum(clf.good_features_)
-            clf.subset_ = self.X.T[clf.good_features_].T
-            print "Finished..."
-
-            # def fit(self,
-            #         traits=None,
-            #         regressor_name=default_regressor_name,
-            #         regressor=default_regressor,
-            #         regressor_kwargs=default_regressor_kwargs):
-            #     raise NotImplementedError("Untested, should be close to working.")
-            #     clf = self.predictor_class(**regressor_kwargs)
-            #     print "Fitting a predictor for trait %s... please wait." % trait
-            #     clf.fit(self.X, self.y[trait])
-            #     self.regressors_[trait][regressor_name] = clf
-            #     print "Finished..."
-
-
 class Classifier(PredictorBase):
     """
     PredictorBase for categorical data
@@ -179,6 +182,8 @@ class Classifier(PredictorBase):
                  score_cutoff_fun=None):
         """Train a classifier on data.
 
+        Parameters
+        ----------
         data : pandas.DataFrame
             Samples x features data to train the predictor on
         trait_data : pandas.Series
@@ -210,9 +215,6 @@ class Classifier(PredictorBase):
         super(Classifier, self).__init__()
 
         # traits encoded to do some work -- "target" variable
-
-
-        # for trait in self.categorical_traits:
         self.traitset = \
             self.trait_data.groupby(self.trait_data).describe().index.levels[0]
         try:
@@ -231,23 +233,3 @@ class Classifier(PredictorBase):
                                     name=self.trait_data.name)
 
 
-    def score(self):
-        """collect scores from classifiers_
-        """
-        sys.stdout.write("Scoring predictor: {} for trait: {}... please "
-                         "wait\n".format(self.name, self.trait))
-
-        self.predictor.scores_ = pd.Series(index=self.X.columns,
-                                           data=self.predictor_scoring_fun(
-                                               self.predictor))
-        self.predictor.score_cutoff_ = \
-            self.score_cutoff_fun(self.predictor.scores_)
-        self.predictor.good_features_ = self.predictor.scores_ > self.predictor.score_cutoff_
-        self.important_features[self.trait][self.name] = self.predictor \
-            .good_features_
-        self.predictor.n_good_features_ = np.sum(
-            self.predictor.good_features_)
-        self.predictor.subset_ = self.X.T[self.predictor.good_features_].T
-
-        sys.stdout.write("\tFinished.\n")
-        self.has_been_scored_yet = True
