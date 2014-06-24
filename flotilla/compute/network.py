7c1e820fc97164795dc0ec3244c4b5bbe05b7bc8
Olga Botvinnik <olga.botvinnik@gmail.com>
2014-06-23 17:54:05 -0700

    reformat
    Former-commit-id: 99366e37184ee8f592cd24071884b8d555d3413e

--- flotilla/compute/network.py
+++ flotilla/compute/network.py
@@ -8,10 +8,10 @@ from ..util import memoize
 
 
 class Networker(object):
-    weight_funs=['abs', 'sq', 'arctan', 'arctan_sq']
+    weight_funs = ['abs', 'sq', 'arctan', 'arctan_sq']
 
     def get_weight_fun(self, fun_name):
-        _abs =  lambda x: x
+        _abs = lambda x: x
         _sq = lambda x: x ** 2
         _arctan = lambda x: np.arctan(x)
         _arctan_sq = lambda x: np.arctan(x) ** 2
@@ -61,7 +61,7 @@ class Networker(object):
         use_cols[n_pcs:] = False
         use_cols = use_cols * np.array(
             [use_pc_1, use_pc_2, use_pc_3, use_pc_4] + [True, ] * (
-            total_pcs - 4))
+                total_pcs - 4))
         selected_cols = data.loc[:, use_cols]
         cov = np.cov(selected_cols)
         nrow, ncol = selected_cols.shape
@@ -71,9 +71,9 @@ class Networker(object):
     @memoize
     def graph(self, adjacency=None, cov_cut=None, name=None,
               node_color_mapper=None,
-                  node_size_mapper=None,
-                  degree_cut = 2,
-                  weight_function='abs'):
+              node_size_mapper=None,
+              degree_cut=2,
+              weight_function='abs'):
 
         if node_color_mapper is None:
             node_color_mapper = self._default_node_color_mapper
@@ -87,23 +87,25 @@ class Networker(object):
                 name = self._last_graph_accessed
         self._last_graph_accessed = name
         try:
-            g,pos = self.graphs_[name]
+            g, pos = self.graphs_[name]
         except:
             wt = self.get_weight_fun(weight_function)
             g = nx.Graph()
             for node_label in adjacency.index:
-
                 node_color = node_color_mapper(node_label)
                 node_size = node_size_mapper(node_label)
-                g.add_node(node_label, node_size=node_size, node_color=node_color)
-        #    g.add_nodes_from(adjacency.index) #to add without setting attributes...neater, but does same thing as above loop
+                g.add_node(node_label, node_size=node_size,
+                           node_color=node_color)
+                #    g.add_nodes_from(adjacency.index) #to add without setting attributes...neater, but does same thing as above loop
             for cell1, others in adjacency.iterrows():
                 for cell2, value in others.iteritems():
                     if value > cov_cut:
                         #cast to floats because write_gml doesn't like numpy dtypes
-                        g.add_edge(cell1, cell2, weight=float(wt(value)),inv_weight=float(1/wt(value)), alpha=0.05)
+                        g.add_edge(cell1, cell2, weight=float(wt(value)),
+                                   inv_weight=float(1 / wt(value)), alpha=0.05)
 
-            g.remove_nodes_from([k for k, v in g.degree().iteritems() if v <= degree_cut])
+            g.remove_nodes_from(
+                [k for k, v in g.degree().iteritems() if v <= degree_cut])
 
             pos = nx.spring_layout(g)
             self.graphs_[name] = (g, pos)
