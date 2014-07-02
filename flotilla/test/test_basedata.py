55211d8009652016997a31818af7a0bd16289cc0
Olga Botvinnik <olga.botvinnik@gmail.com>
2014-07-02 15:09:11 -0700

    move tests out of source directory

--- flotilla/test/test_basedata.py
+++
@@ -1,8 +0,0 @@
-__author__ = 'olga'
-
-from flotilla.data_model.base import BaseData
-import pandas.util.testing as pdt
-
-def test_basedata_init(example_data):
-    base_data = BaseData(example_data.expression)
-    pdt.assert_frame_equal(base_data.data, example_data.expression)
--- flotilla/test/test_ipython_interact.py
+++
@@ -1,3 +0,0 @@
-"""
-This tests whether the IPython interactive modules are being created correctly
-"""
