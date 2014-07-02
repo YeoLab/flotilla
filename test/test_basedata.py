78bb2052b0a0f767bc1bdd810f26c568cc142d27
Olga Botvinnik <olga.botvinnik@gmail.com>
2014-07-02 16:32:01 -0700

    move test files
    Former-commit-id: 90e631a1b96dd55cfaa6741d4ff1b7e16a3d297a

---
+++ test/test_basedata.py
@@ -0,0 +1,10 @@
+__author__ = 'olga'
+
+import pandas.util.testing as pdt
+
+from flotilla.data_model.base import BaseData
+
+
+def test_basedata_init(example_data):
+    base_data = BaseData(example_data.expression)
+    pdt.assert_frame_equal(base_data.data, example_data.expression)
