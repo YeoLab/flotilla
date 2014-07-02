2af95e3c9a9969008b47e241c28d495ae9c8bb81
Olga Botvinnik <olga.botvinnik@gmail.com>
2014-07-02 15:24:17 -0700

    Initial creation of modalities object

--- flotilla/compute/splicing.py
+++ flotilla/compute/splicing.py
@@ -1,1 +1,17 @@
-__author__ = 'olga'
+import numpy as np
+import pandas as pd
+
+
+class Modalities(object):
+    modalities_bins = np.array([[1, 0, 0],  # excluded
+                                [0, 1, 0],  # middle
+                                [0, 0, 1],  # included
+                                [1, 0, 1],  # bimodal
+                                [1, 1, 1]])  # uniform
+
+    modalities_names = ['included', 'middle', 'excluded', 'bimodal', 'uniform']
+
+    true_modalities = pd.DataFrame(modalities_bins, index=modalities_names)
+
+    def __init__(self):
+        pass
