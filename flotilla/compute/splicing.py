46eae3b926c7583f6028fd9442297c5c27f235e1
Olga Botvinnik <olga.botvinnik@gmail.com>
2014-07-03 15:54:31 -0700

    Modalities seem to work, still need to tie to SplicingData
    Former-commit-id: c51dfa700051e8af29d25f606863c3822f0217be

--- flotilla/compute/splicing.py
+++ flotilla/compute/splicing.py
@@ -1,7 +1,7 @@
 import numpy as np
 import pandas as pd
 
-from .infotheory import jsd
+from .infotheory import jsd, binify
 
 
 class Modalities(object):
@@ -13,17 +13,27 @@ class Modalities(object):
 
     modalities_names = ['included', 'middle', 'excluded', 'bimodal', 'uniform']
 
-    true_modalities = pd.DataFrame(modalities_bins, index=modalities_names)
+    true_modalities = pd.DataFrame(modalities_bins.T, columns=modalities_names)
 
-    def __init__(self):
-        pass
+    def __init__(self, psi, excluded_max=0.2, included_min=0.8):
+        self.bins = (0, excluded_max, included_min, 1)
+
+        self.binned = binify(psi, self.bins)
+        self.true_modalities.index = self.binned.index
 
     def _col_jsd_modalities(self, col):
         return self.true_modalities.apply(lambda x: jsd(x, col), axis=0)
 
-    def sqrt_jsd_modalities(self, binned):
+    @property
+    def sqrt_jsd_modalities(self):
         """Return the square root of the JSD of each splicing event versus
         all the modalities. Use square root of JSD because it's a metric.
 
         """
-        return binned.apply(self._col_jsd_modalities, axis=0)
+        return np.sqrt(self.binned.apply(self._col_jsd_modalities, axis=0))
+
+    @property
+    def modality_assignments(self):
+        return self.true_modalities.columns[
+            np.argmin(self.sqrt_jsd_modalities.values, axis=0)]
+
