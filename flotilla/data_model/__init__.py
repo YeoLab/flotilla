174a9e610b0edd044a3b72d33ee59bfebfdb5fb1
Olga Botvinnik <olga.botvinnik@gmail.com>
2014-07-09 15:07:06 -0700

    fix ExpressionData import

--- flotilla/data_model/__init__.py
+++ flotilla/data_model/__init__.py
@@ -1,5 +1,6 @@
 __author__ = 'olga'
 
+from .expression import ExpressionData, SpikeInData
 from .metadata import MetaData
 from .quality_control import MappingStatsData
 from .splicing import SplicingData, SpliceJunctionData, DownsampledSplicingData
