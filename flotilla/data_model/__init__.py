aad47a6241ad5ff9cb39453c527bc83925a11e29
Olga Botvinnik <olga.botvinnik@gmail.com>
2014-07-09 12:55:24 -0700

    fix newline, space after comments for pep8

--- flotilla/data_model/__init__.py
+++ flotilla/data_model/__init__.py
@@ -1,7 +1,6 @@
 __author__ = 'olga'
 
 from .experiment_design import ExperimentDesignData
-from .expression import ExpressionData, SpikeInData
 from .quality_control import MappingStatsData
 from .splicing import SplicingData, SpliceJunctionData, DownsampledSplicingData
 from .study import Study, StudyFactory
