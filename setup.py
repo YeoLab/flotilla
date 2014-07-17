49de5eb67f0033d22e6b322261def405784a9f35
Olga Botvinnik <olga.botvinnik@gmail.com>
2014-07-17 10:49:45 -0700

    trying to fix dependency on clustering2 fork of seaborn

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setup(
     description='Embark on a journey of data exploration.',
     install_requires=open('requirements.txt').readlines(),
     dependency_links=[
-        'http://github.com/olgabot/seaborn/tarball/clustering2#egg=seaborn-0'
-        '.4.clustering'],
+        'git+ssh://git@github.com/olgabot/seaborn/tarball/clustering2#egg'
+        '=seaborn-0.4.clustering'],
     version="0.0.4"
 )
