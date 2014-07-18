9e187020577a37a75f03786bff6abfa80d4b3e45
Olga Botvinnik <olga.botvinnik@gmail.com>
2014-07-17 23:07:00 -0700

    fix test_expressiondata stuff, move test_subset to test_basedata and parameterized
    Former-commit-id: ee1fe4162c9ccdb577b561930a81b9628f3c7405

--- test/test_expressiondata.py
+++ test/test_expressiondata.py
@@ -15,35 +15,6 @@ class TestExpressionData:
             expression.data > ExpressionData._expression_thresh]
         pdt.assert_frame_equal(expression.sparse_data, sparse_data)
 
-
-    def test_subset(self):
-        pass
-
-    def test__subset_and_standardize(self, expression):
-        # TODO: parameterize and test with feature/sample subsets and
-        # with/without standardization
-        from sklearn.preprocessing import StandardScaler
-        import pandas as pd
-
-        expression.subset, expression.means = \
-            expression._subset_and_standardize(expression.sparse_data,
-                                               return_means=True)
-
-        columns = expression.sparse_data.count() > expression.min_samples
-        subset = expression.sparse_data.ix[:, columns]
-        means = subset.mean().rename_axis(expression.feature_renamer)
-        subset = subset.fillna(means).fillna(0)
-        subset = subset.rename_axis(expression.feature_renamer, 1)
-
-        data = StandardScaler().fit_transform(subset)
-
-        subset_standardized = pd.DataFrame(data, index=subset.index,
-                                           columns=subset.columns)
-
-        pdt.assert_frame_equal(subset_standardized, expression.subset)
-        pdt.assert_series_equal(means, expression.means)
-
-
     def test_reduce(self, example_data):
         #TODO: parameterize and test with featurewise and subsets
         expression = ExpressionData(example_data.expression)
