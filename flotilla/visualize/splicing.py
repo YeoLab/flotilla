7ff71eca899e0007a4def0ab8093f309603ce95e
Olga Botvinnik <olga.botvinnik@gmail.com>
2014-07-28 10:04:28 -0700

    Move violinplot into visualize.generic with option for splicing or expression data
    Former-commit-id: dcf3c3a1735c53ae54199ce374142f3a3344081f

--- flotilla/visualize/splicing.py
+++ flotilla/visualize/splicing.py
@@ -258,34 +258,4 @@ def plot_pooled_dot(ax, pooled, x_offset=0, label=True):
                         xytext=(7, 0), fontsize=14)
 
 
-def psi_violinplot(psi, groupby=None, color=None, ax=None, pooled_psi=None,
-                   order=None, violinplot_kws=None, title=None,
-                   label_pooled=True):
-    if ax is None:
-        ax = plt.gca()
 
-    violinplot_kws = {} if violinplot_kws is None else violinplot_kws
-
-    # Add a tiny amount of random noise in case the values are all identical,
-    # Otherwise we get a LinAlg error.
-    psi += np.random.uniform(0, 0.001, psi.shape[0])
-    sns.violinplot(psi, groupby=groupby, bw=0.1, inner='points',
-                   color=color, linewidth=0.5, order=order,
-                   ax=ax, **violinplot_kws)
-    if pooled_psi is not None:
-        grouped = pooled_psi.groupby(groupby)
-        if order is not None:
-            for i, name in enumerate(order):
-                try:
-                    subset = pooled_psi.ix[grouped.groups[name]]
-                    plot_pooled_dot(ax, subset, x_offset=i, label=label_pooled)
-                except KeyError:
-                    pass
-        else:
-            plot_pooled_dot(ax, pooled_psi)
-    ax.set_ylim(0, 1)
-    ax.set_yticks([0, 0.5, 1])
-    ax.set_ylabel('$\Psi$')
-    if title is not None:
-        ax.set_title(title)
-    sns.despine()
