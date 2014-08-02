921739149bcce4afaf2ce3d4833f19f2158a1867
Olga Botvinnik <olga.botvinnik@gmail.com>
2014-08-01 17:16:35 -0700

    Add outliers to violinplots, abstract out actual violniplot making
    Former-commit-id: a1447f0d335bcfa820ae7b2fb3b5b86f410e2e29

--- flotilla/visualize/generic.py
+++ flotilla/visualize/generic.py
@@ -5,11 +5,15 @@ import seaborn as sns
 
 def violinplot(data, groupby=None, color=None, ax=None, pooled_data=None,
                order=None, violinplot_kws=None, title=None,
-               label_pooled=True, outliers=None, data_type='splicing'):
+               label_pooled=True, outliers=None, data_type=None):
     """
     Parameters
     ----------
-
+    data : pandas.Series
+        The main data to plot
+    groupby : dict-like
+        How to group the samples (e.g. by phenotype)
+    color : list
 
     Returns
     -------
@@ -19,32 +23,19 @@ def violinplot(data, groupby=None, color=None, ax=None, pooled_data=None,
     ------
     """
     data_type = 'none' if data_type is None else data_type
-
     splicing = 'splicing'.startswith(data_type)
+
+    violinplot_kws = {} if violinplot_kws is None else violinplot_kws
+    violinplot_kws.setdefault('alpha', 0.75)
 
     if ax is None:
         ax = plt.gca()
 
-    violinplot_kws = {} if violinplot_kws is None else violinplot_kws
+    _violinplot_group(data, groupby=groupby, color=color, ax=ax, order=order,
+                      violinplot_kws=violinplot_kws, splicing=splicing)
+    import pdb;
 
-    # Add a tiny amount of random noise in case the values are all identical,
-    # Otherwise we get a LinAlg error.
-    data += np.random.uniform(0, 0.001, data.shape[0])
-
-    # Check that all the groups are represented, if not, add some data out of
-    # range to the missing group
-    if groupby is not None and order is not None:
-        validated_groups = data.groupby(groupby).size().keys()
-        verified_order = [x for x in order if x in validated_groups]
-        positions = [i for i, x in enumerate(order) if x in validated_groups]
-    else:
-        verified_order = order
-        positions = None
-
-    inner = 'points' if splicing else 'box'
-    sns.violinplot(data, groupby=groupby, bw=0.1, inner=inner,
-                   color=color, linewidth=0.5, order=verified_order,
-                   ax=ax, positions=positions, **violinplot_kws)
+    pdb.set_trace()
     if pooled_data is not None:
         grouped = pooled_data.groupby(groupby)
         if order is not None:
@@ -58,15 +49,13 @@ def violinplot(data, groupby=None, color=None, ax=None, pooled_data=None,
             plot_pooled_dot(ax, pooled_data)
 
     if outliers is not None:
-        if groupby is not None and order is not None:
-            outlier_groups = outliers.groupby(groupby).size().keys()
-            outlier_order = [x for x in order if x in outlier_groups]
-            outlier_positions = [i for i, x in enumerate(order) if
-                                 x in outlier_groups]
-        sns.violinplot(outliers, groupby=groupby, bw=0.1, inner=inner,
-                       color='lightgrey', linewidth=0.5, order=outlier_order,
-                       ax=ax, positions=outlier_positions,
-                       **violinplot_kws)
+        outlier_violinplot_kws = violinplot_kws
+
+        # make sure this is behind the non outlier data
+        outlier_violinplot_kws['zorder'] = -1
+        _violinplot_group(outliers, groupby=groupby, color='lightgrey', ax=ax,
+                          order=order, violinplot_kws=outlier_violinplot_kws,
+                          splicing=splicing)
 
     if splicing:
         ax.set_ylim(0, 1)
@@ -75,6 +64,9 @@ def violinplot(data, groupby=None, color=None, ax=None, pooled_data=None,
 
     if title is not None:
         ax.set_title(title)
+
+    if order is not None:
+        ax.set_xlim(-.5, len(order) + .5)
 
     if groupby is not None and order is not None:
         sizes = data.groupby(groupby).size()
@@ -87,7 +79,74 @@ def violinplot(data, groupby=None, color=None, ax=None, pooled_data=None,
     sns.despine()
 
 
+def _violinplot_group(data, groupby=None, order=None, violinplot_kws=None,
+                      color=None, ax=None, splicing=False):
+    """Plot a single groups violinplot.
+
+    Separated out so real data plotting and outlier plotting works the same
+    """
+    single_points = data.groupby(groupby).filter(lambda x: len(x) < 2)
+
+    data = data.groupby(groupby).filter(lambda x: len(x) > 1)
+
+    # Check that all the groups are represented, if not, add some data out of
+    # range to the missing group
+    if groupby is not None and order is not None:
+        validated_groups = data.groupby(groupby).size().keys()
+        verified_order = [x for x in order if x in validated_groups]
+        positions = [i for i, x in enumerate(order) if x in validated_groups]
+
+        single_groups = single_points.groupby(groupby).size().keys()
+        single_positions = dict((x, i) for i, x in enumerate(order) if
+                                x in single_groups)
+        single_color = dict((group, color) for i, (color, group) in
+                            enumerate(zip(color, single_groups))
+                            if group in single_groups)
+    else:
+        verified_order = order
+        positions = None
+
+        single_positions = None
+        single_color = None
+
+    violinplot_kws = {} if violinplot_kws is None else violinplot_kws
+
+    # Add a tiny amount of random noise in case the values are all identical,
+    # Otherwise we get a LinAlg error.
+    data += np.random.uniform(0, 0.001, data.shape[0])
+
+    print 'positions', positions
+    inner = 'points' if splicing else 'box'
+    sns.violinplot(data, groupby=groupby, bw=0.1, inner=inner,
+                   color=color, linewidth=0.5, order=verified_order,
+                   ax=ax, positions=positions, **violinplot_kws)
+
+    # if single_points is not None:
+    #     for group, y in single_points.groupby(groupby):
+    #         x = single_positions[group]
+    #         c = single_color[group]
+    #         ax.plot([x], [y], 'o', color=c)
+    #         ax.annotate(y.index[0], (x, y), textcoords='offset points',
+    #                     xytext=(7, 0), fontsize=14)
+
+
 def plot_pooled_dot(ax, pooled, x_offset=0, label=True):
+    """
+    Parameters
+    ----------
+    ax : matplotlib.axes.Axes
+        Axes object to plot on
+    pooled : pandas.Series
+        Pooled data of this gene
+
+    Returns
+    -------
+
+
+    Raises
+    ------
+    """
+    pooled = pooled.dropna()
     try:
         xs = np.ones(pooled.shape[0])
     except AttributeError:
