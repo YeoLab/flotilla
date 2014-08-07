710b20ad526bdf350116089e1233edf47af684e1
Olga Botvinnik <olga.botvinnik@gmail.com>
2014-08-07 16:43:30 -0700

    Move Study properties to MetaData
    Former-commit-id: 2d9aadcaa543a4305b47fd596e0f9ca20817de87

--- flotilla/data_model/metadata.py
+++ flotilla/data_model/metadata.py
@@ -1,20 +1,28 @@
+import itertools
 import sys
 
 import matplotlib as mpl
+import pandas as pd
 
 from .base import BaseData
-from ..visualize.color import str_to_color
+from ..visualize.color import str_to_color, set1
+
 
 
 # Any informational data goes here
 
 class MetaData(BaseData):
     def __init__(self, data, phenotype_order=None, phenotype_to_color=None,
-                 phenotype_to_marker=None, predictor_config_manager=None, ):
+                 phenotype_to_marker=None, phenotype_col='phenotype',
+                 predictor_config_manager=None):
         super(MetaData, self).__init__(data, outliers=None,
                                        predictor_config_manager=predictor_config_manager)
+
+        self.phenotype_col = phenotype_col
         self.phenotype_order = phenotype_order
         self.phenotype_to_color = phenotype_to_color
+
+        # Convert color strings to non-default matplotlib colors
         if self.phenotype_to_color is not None:
             for phenotype, color in self.phenotype_to_color.iteritems():
                 try:
@@ -32,26 +40,38 @@ class MetaData(BaseData):
                         'falling back on "o" (circle)'.format(marker))
                     self.phenotype_to_marker[phenotype] = 'o'
 
-    def _get(self, sample_metadata_filename=None, gene_metadata_filename=None,
-             event_metadata_filename=None):
+    @property
+    def phenotype_order(self):
+        return self._phenotype_order
 
-        metadata = {'sample': None,
-                    'gene': None,
-                    'event': None}
-        try:
-            if sample_metadata_filename is not None:
-                metadata['sample'] = self.load(*sample_metadata_filename)
-            if event_metadata_filename is not None:
-                metadata['event'] = self.load(*event_metadata_filename)
-            if gene_metadata_filename is not None:
-                metadata['gene'] = self.load(*gene_metadata_filename)
+    @phenotype_order.setter
+    def phenotype_order(self, value):
+        if value is not None:
+            self._phenotype_order = value
+        else:
+            self._phenotype_order = list(sorted(self.sample_id_to_phenotype
+                                                .unique()))
 
-        except Exception as E:
-            sys.stderr.write("error loading descriptors: %s, \n\n .... "
-                             "entering pdb ... \n\n" % E)
-            raise E
+    @property
+    def phenotype_to_color(self):
+        return self._phenotype_to_color
 
-        return {'experiment_design_data': metadata['sample'],
-                'feature_data': metadata['gene'],
-                'event_metadata': metadata['event'],
-                'expression_metadata': None}
+    @phenotype_to_color.setter
+    def phenotype_to_color(self, value):
+        if value is not None:
+            self._phenotype_to_color = value
+        else:
+            self._phenotype_to_color = dict(zip(self.phenotype_order,
+                                                itertools.cycle(set1)))
+
+    @property
+    def phenotype_color_order(self):
+        return [self.phenotype_to_color[p] for p in self.phenotype_order]
+
+    @property
+    def sample_id_to_phenotype(self):
+        if self.phenotype_col in self.data:
+            return self.data[self.phenotype_col]
+        else:
+            return pd.Series([self.phenotype_col],
+                             index=self.data.index)
