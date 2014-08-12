f9a580e0cb466120fbd90aff4bf6da1cab5d5c94
Olga Botvinnik <olga.botvinnik@gmail.com>
2014-08-12 11:06:02 -0700

    add natural sort function

--- flotilla/util.py
+++ flotilla/util.py
@@ -7,6 +7,7 @@ general utilities
 from functools import wraps
 import errno
 import os
+import re
 import signal
 import sys
 import subprocess
@@ -169,3 +170,13 @@ def as_numpy(x):
     except AttributeError:
         # Numpy array
         return x
+
+
+def natural_sort(l):
+    """
+    From
+    http://stackoverflow.com/a/4836734
+    """
+    convert = lambda text: int(text) if text.isdigit() else text.lower()
+    alphanum_key = lambda key: [convert(c) for c in re.split('([0-9]+)', key)]
+    return sorted(l, key=alphanum_key)
