b9ec9847121fbe5f0c3cf12852bb99e5fbb29124
Olga Botvinnik <olga.botvinnik@gmail.com>
2014-08-12 11:11:07 -0700

    fix setup.py?
    Former-commit-id: c13a6bcfb5f0261a9ebed2a32f5a52be49ae37a2

--- setup.py
+++ setup.py
@@ -7,12 +7,8 @@ setup(
     url='http://github.com/YeoLab/flotilla',
     license='',
     author='mlovci,olgabot',
-    author_email='mlovci@ucsd.edu',
+    author_email='obotvinn@ucsd.edu',
     description='Embark on a journey of data exploration.',
-    install_requires=['pandas', 'scikit-learn', 'seaborn==0.4clustering',
-                      'networkx', 'fastcluster'],
-    dependency_links=[
-        'git+ssh://git@github.com/olgabot/seaborn/tarball/clustering2#egg'
-        '=seaborn-0.4.clustering'],
+    install_requires=open('requirements.txt').readlines(),
     version="0.0.4"
 )
