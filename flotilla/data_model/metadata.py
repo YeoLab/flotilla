9b9fa84d3e32da21b8b73fd2ba2ac4ef694a51a1
Olga Botvinnik <olga.botvinnik@gmail.com>
2014-08-22 11:57:21 -0700

    add and fix tests for study if columns in the datapackage aren't specified
    Former-commit-id: 42871a4f0c1db662b0658907eabeb9a4e09cd3fc

--- flotilla/data_model/metadata.py
+++ flotilla/data_model/metadata.py
@@ -2,28 +2,42 @@ import itertools
 import sys
 
 import matplotlib as mpl
-import pandas as pd
+import seaborn as sns
 
 from .base import BaseData
 from ..visualize.color import str_to_color, set1
 
 
 
-
-
-
 # Any informational data goes here
 
 class MetaData(BaseData):
+    _default_phenotype_col = 'phenotype'
+    _default_pooled_col = 'pooled'
+
     def __init__(self, data, phenotype_order=None, phenotype_to_color=None,
-                 phenotype_to_marker=None, phenotype_col='phenotype',
+                 phenotype_to_marker=None,
+                 phenotype_col=_default_phenotype_col,
+                 pooled_col=None,
                  predictor_config_manager=None):
         super(MetaData, self).__init__(data, outliers=None,
                                        predictor_config_manager=predictor_config_manager)
 
-        self.phenotype_col = phenotype_col
+        self.phenotype_col = phenotype_col if phenotype_col is not None else \
+            self._default_phenotype_col
         self.phenotype_order = phenotype_order
         self.phenotype_to_color = phenotype_to_color
+        self.pooled_col = pooled_col
+
+        if self.phenotype_col not in self.data:
+            sys.stderr.write('The required column name "{}" does not exist in '
+                             'the sample metadata. All samples will be '
+                             'treated as the same phenotype. You may also '
+                             'specify "phenotype_col" in the metadata section '
+                             'of the datapackge.'.format(self.phenotype_col))
+            self.data[self.phenotype_col] = 'phenotype'
+            self.phenotype_order = None
+            self.phenotype_to_color = None
 
         # Convert color strings to non-default matplotlib colors
         if self.phenotype_to_color is not None:
@@ -33,6 +47,13 @@ class MetaData(BaseData):
                     self.phenotype_to_color[phenotype] = color
                 except KeyError:
                     pass
+        else:
+            sys.stderr.write('No phenotype to color mapping was provided, '
+                             'so coming up with reasonable defaults')
+            self.phenotype_to_color = {}
+            colors = sns.color_palette(n_colors=self.n_phenotypes)
+            for phenotype, color in zip(self.unique_phenotypes, colors):
+                self.phenotype_to_color[phenotype] = color
 
         self.phenotype_to_marker = phenotype_to_marker
         if self.phenotype_to_marker is not None:
@@ -42,6 +63,20 @@ class MetaData(BaseData):
                         '{} is not a valid matplotlib marker style, '
                         'falling back on "o" (circle)'.format(marker))
                     self.phenotype_to_marker[phenotype] = 'o'
+        else:
+            sys.stderr.write('No phenotype to marker (matplotlib plotting '
+                             'symbol) was provided, so each phenotype will be '
+                             'plotted as a circle in the PCA visualizations.')
+            self.phenotype_to_marker = dict.fromkeys(
+                self.sample_id_to_phenotype.unique(), 'o')
+
+    @property
+    def n_phenotypes(self):
+        return len(self.self.unique_phenotypes)
+
+    @property
+    def unique_phenotypes(self):
+        return self.sample_id_to_phenotype.unique()
 
     @property
     def phenotype_order(self):
@@ -52,8 +87,7 @@ class MetaData(BaseData):
         if value is not None:
             self._phenotype_order = value
         else:
-            self._phenotype_order = list(sorted(self.sample_id_to_phenotype
-                                                .unique()))
+            self._phenotype_order = list(sorted(self.unique_phenotypes))
 
     @property
     def phenotype_to_color(self):
@@ -73,11 +107,7 @@ class MetaData(BaseData):
 
     @property
     def sample_id_to_phenotype(self):
-        if self.phenotype_col in self.data:
-            return self.data[self.phenotype_col]
-        else:
-            return pd.Series([self.phenotype_col],
-                             index=self.data.index)
+        return self.data[self.phenotype_col]
 
     @property
     def sample_id_to_color(self):
--- test/test_study.py
+++ test/test_study.py
@@ -2,6 +2,9 @@
 This tests whether the Study object was created correctly. No
 computation or visualization tests yet.
 """
+import json
+import os
+
 import pandas.util.testing as pdt
 import pytest
 
@@ -45,6 +48,69 @@ class TestStudy(object):
 
     def test_plot_classifier(self, study):
         study.plot_classifier('P_cell')
+
+    @pytest.fixture(scope="module", params=[None, 'pooled_col',
+                                            'phenotype_col'])
+    def metadata_key(self, request):
+        return request.param
+
+    @pytest.fixture(params=[None, 'feature_rename_col'])
+    def expression_key(self, request):
+        return request.param
+
+    @pytest.fixture(params=[None, 'feature_rename_col'])
+    def splicing_key(self, request):
+        return request.param
+
+    @pytest.fixture
+    def datapackage(self, example_datapackage_path, metadata_key,
+                    expression_key, splicing_key):
+        with open(example_datapackage_path) as f:
+            datapackage = json.load(f)
+        datatype_to_key = {'metadata': metadata_key,
+                           'expression': expression_key,
+                           'splicing': splicing_key}
+        for datatype, key in datatype_to_key.iteritems():
+            if key is not None:
+                resources = datapackage['resources']
+                resource = [r for r in resources
+                            if r['name'] == datatype][0]
+                if key in resource:
+                    resource.pop(key)
+        return datapackage
+
+    @pytest.fixture
+    def datapackage_dir(self, example_datapackage_path):
+        return os.path.dirname(example_datapackage_path)
+
+    def test_from_datapackage(self, datapackage, datapackage_dir):
+        import flotilla
+
+        study = flotilla.Study.from_datapackage(datapackage, datapackage_dir)
+
+        metadata_resource = [r for r in datapackage['resources']
+                             if r['name'] == 'metadata'][0]
+        expression_resource = [r for r in datapackage['resources']
+                               if r['name'] == 'expression'][0]
+        splicing_resource = [r for r in datapackage['resources']
+                             if r['name'] == 'splicing'][0]
+
+        phenotype_col = 'phenotype' if 'phenotype_col' \
+            not in metadata_resource else metadata_resource['phenotype_col']
+        pooled_col = None if 'pooled_col' not in metadata_resource else \
+            metadata_resource['pooled_col']
+        expression_feature_rename_col = 'gene_name' if \
+            'feature_rename_col' not in expression_resource \
+            else expression_resource['feature_rename_col']
+        splicing_feature_rename_col = 'gene_name' if \
+            'feature_rename_col' not in splicing_resource \
+            else splicing_resource['feature_rename_col']
+
+        assert study.metadata.phenotype_col == phenotype_col
+        assert study.metadata.pooled_col == pooled_col
+        assert study.expression.feature_rename_col \
+               == expression_feature_rename_col
+        assert study.splicing.feature_rename_col == splicing_feature_rename_col
 
 
 # def test_write_package(tmpdir):
