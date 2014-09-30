b9b0d125ff35f44ef977104fb6d214b5b28494e2
Olga Botvinnik <olga.botvinnik@gmail.com>
2014-09-30 15:20:53 -0700

    fix PyPI classifier
    Former-commit-id: 41e4ab8672835ec1cfca1a1fd243cbfae54e85ba

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ setup(
     classifiers=['License :: OSI Approved :: MIT License',
                  'Topic :: Scientific/Engineering :: Bio-Informatics',
                  'Topic :: Scientific/Engineering :: Visualization',
-                 'Topic :: Scientific/Engineering :: Medical Science Apps.,'
+                 'Topic :: Scientific/Engineering :: Medical Science Apps.',
                  'Programming Language :: Python :: 2.7',
                  'Topic :: Multimedia :: Graphics',
                  'Operating System :: POSIX',
