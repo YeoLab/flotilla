0e42a87353fb8714c88ce3811bc7ab905fa17573
Olga Botvinnik <olga.botvinnik@gmail.com>
2014-10-30 11:17:59 -0700

    cleanup commented code

--- flotilla/compute/decomposition.py
+++ flotilla/compute/decomposition.py
@@ -103,13 +103,3 @@ class DataFrameNMF(DataFrameReducerBase, decomposition.NMF):
 
 class DataFrameICA(DataFrameReducerBase, decomposition.FastICA):
     pass
-
-
-# def L1_distance(x, y):
-#     """Really should just be using TODO:scipy.linalg.norm with order=1"""
-#     return abs(y) + abs(x)
-#
-#
-# def L2_distance(x, y):
-#     """Really should just be using TODO:scipy.linalg.norm with order=2"""
-#     return math.sqrt((y ** 2) + (x ** 2))
