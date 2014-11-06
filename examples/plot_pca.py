13d0fdf06ad5cd9fcba6fb47ff037bcd17974cd8
Olga Botvinnik <olga.botvinnik@gmail.com>
2014-11-06 05:34:53 -0500

    remove violins from pca
    Former-commit-id: 44166f49b9e675963708e3d91314ea0619bfbf9b

--- examples/plot_pca.py
+++ examples/plot_pca.py
@@ -1,8 +1,8 @@
 """
-Visualize principal components analysis dimensionality reduction
+Visualize principal component analysis dimensionality reduction
 ================================================================
 
 """
 import flotilla
 study = flotilla.embark(flotilla._neural_diff_chr22)
-study.plot_pca()
+study.plot_pca(plot_violins=False)
