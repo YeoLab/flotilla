b8cba160a012c81dc889fd7b53ff16b7f53a2946
Olga Botvinnik <olga.botvinnik@gmail.com>
2014-11-08 11:53:35 -0500

    move logic of whether or not to deploy docs and the name of the directory to .travis/setup.sh

--- .travis/setup.sh
+++ .travis/setup.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-#setup travis-ci configuration basing one the being-built branch
+# setup travis-ci configuration basing one the being-built branch
+
+export DEPLOY_DOCS=false
 
 if [[ $TRAVIS_BRANCH == 'master' ]] ; then
-    export DEPLOY_HTML_DIR=docs
-elif [[ $TRAVIS_BRANCH == 'develop' ]] ; then
-    export DEPLOY_HTML_DIR=docs/develop
+    export DEPLOY_HTML_DIR=docs-dev
+    export DEPLOY_DOCS=true
 elif [[ $TRAVIS_BRANCH =~ ^v[0-9.]+$ ]]; then
-    export DEPLOY_HTML_DIR=docs/${TRAVIS_BRANCH:1}
-else
-    export DEPLOY_HTML_DIR=docs/$TRAVIS_BRANCH
+    export DEPLOY_HTML_DIR=docs
+    export DEPLOY_DOCS=true
 fi
