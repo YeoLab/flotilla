dd439ff5e2ecaeb53eeb274f8f6a5afe8a34a81d
Olga Botvinnik <olga.botvinnik@gmail.com>
2014-11-12 19:32:41 -0500

    remove printing messages from violinplot
    Former-commit-id: 2ae8f0c6992ec74a757009ac567335c7d47df2ff

--- flotilla/visualize/generic.py
+++ flotilla/visualize/generic.py
@@ -125,12 +125,6 @@ def _violinplot_single_dataset(data, groupby=None, order=None,
         single_positions = None
         single_color = None
 
-    print data.name, data.shape
-    print 'verified_color', verified_color
-    print 'verified_order', verified_order
-    print data.groupby(groupby).groups.keys()
-    print
-
     violinplot_kws = {} if violinplot_kws is None else violinplot_kws
 
     # Add a tiny amount of random noise in case the values are all identical,
