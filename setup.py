615cc7d8e554a8df396e39facd97191ca5b7ffc7
Michael Lovci <michaeltlovci@gmail.com>
2014-11-21 18:05:05 -0800

    Update setup.py
    added joblib to setup.py dependencies

--- setup.py
+++ setup.py
@@ -41,6 +41,7 @@ setup(
                       "jinja2",
                       #"fastcluster",
                       "semantic_version",
+                      "joblib",
     ],
     version=version,
     classifiers=['License :: OSI Approved :: BSD License',
