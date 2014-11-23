ae5a2dfada087ceea5e3eff94042e3182f1bfff2
Olga Botvinnik <olga.botvinnik@gmail.com>
2014-11-23 02:20:53 -0500

    add "see also" for plot_{pca,clustermap,graph,corelations} examples
    Former-commit-id: 8e2ad558e62bd208f273abbee2658f71f9a27e45

--- examples/plot_pca.py
+++ examples/plot_pca.py
@@ -2,6 +2,10 @@
 Visualize principal component analysis dimensionality reduction
 ================================================================
 
+See also
+--------
+:py:func:`Study.interactive_pca`
+
 """
 import flotilla
 study = flotilla.embark(flotilla._shalek2013)
