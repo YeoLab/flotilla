88466b1cdf32d89cbfc0de61be442a48b9e266fe
Olga Botvinnik <olga.botvinnik@gmail.com>
2014-12-23 12:03:04 -0800

    initial remaking of independent test datasets
    Former-commit-id: 5f473cacf5c1ab08970b6f26671ba9caf1274b91

--- flotilla/test/data_model/test_basedata.py
+++ flotilla/test/data_model/test_basedata.py
@@ -1,12 +1,54 @@
+import numpy as np
 import numpy.testing as npt
 import pandas.util.testing as pdt
 
+# @pytest.fixture(params=['expression', 'splicing'])
+# def data_type(request):
+#     return request.param
+#
+# @pytest.fixture
+# def data(data_type, expression_data, splicing_data):
+#     if data_type == 'expression':
+#         return expression_data
+#     elif data_type == 'splicing':
+#         return splicing_data
+#
+# @pytest.fixture
+# def feature_data(data_type, expression_feature_data, splicing_feature_data):
+#     if data_type == 'expression':
+#         return expression_feature_data
+#     elif data_type == 'splicing':
+#         return splicing_feature_data
+#
+# @pytest.fixture
+# def thresh(data_type, expression_thresh):
+#     if data_type == 'expression':
+#         return expression_thresh
+#     else:
+#         return -np.inf
+#
+# @pytest.fixture
+# def feature_rename_col(data_type, )
+
 
 class TestBaseData:
-    def test_basedata_init(self, shalek2013_data):
+    def test_basedata_init(self, expression_data, expression_feature_data,
+                           expression_thresh, expression_feature_rename_col,
+                           metadata_minimum_samples):
         from flotilla.data_model.base import BaseData
-        base_data = BaseData(shalek2013_data.expression)
-        pdt.assert_frame_equal(base_data.data, shalek2013_data.expression)
+        base_data = BaseData(expression_data,
+                             feature_data=expression_feature_data,
+                             thresh=expression_thresh,
+                             minimum_samples=metadata_minimum_samples,
+                             feature_rename_col=expression_feature_rename_col)
+        if expression_thresh > -np.inf or metadata_minimum_samples > 0:
+            data = base_data._threshold(expression_data)
+
+        pdt.assert_frame_equal(base_data.data_original, expression_data)
+        pdt.assert_frame_equal(base_data.feature_data, expression_feature_data)
+        pdt.assert_frame_equal(base_data.data, data)
+        pdt.assert_frame_equal(base_data.feature_renamer_series,
+                               )
 
     def test_subset(self, base_data, sample_ids, feature_ids):
         import pandas as pd
