624f3b1f3b6fbef46ef169394daa812209a40903
Olga Botvinnik <olga.botvinnik@gmail.com>
2015-02-02 17:11:55 -0800

    initial commit for gene ontology

---
+++ flotilla/data_model/gene_ontology.py
@@ -0,0 +1,115 @@
+from collections import defaultdict
+from itertools import izip
+
+import pandas as pd
+from scipy.stats import hypergeom
+
+
+class GeneOntologyData(object):
+
+    def __init__(self, data):
+
+        self.data = data
+
+        ontology, all_genes = self._generate_ontology(self.data)
+        self.ontology = ontology
+        self.all_genes = all_genes
+
+    def _generate_ontology(self, df):
+
+        ontology = defaultdict(
+            lambda: {'genes': set(), 'name': set(), 'domain': set()})
+        allGenesInOntologies = set(df.get('Ensembl Gene ID'))
+        for GO, gene, domain, name in izip(
+                df.get('GO Term Accession'),
+                df.get('Ensembl Gene ID'),
+                df.get('GO domain'),
+                df.get('GO Term Name')):
+            ontology[GO]['genes'].add(gene)
+            ontology[GO]['name'].add(name)
+            ontology[GO]['domain'].add(domain)
+            ontology[GO]['n_genes'] = len(ontology[GO]['genes'])
+        return ontology, allGenesInOntologies
+
+
+    def enrichment(self, features_of_interest, background=None,
+                   p_value_cutoff=1000000, xRef={}):
+        background = self.all_genes if background is None else background
+        n_all_genes = len(background),
+        n_features_of_interest = len(features_of_interest)
+        pValues = defaultdict()
+        n_comparisons = 0
+
+        for go_term, go_genes in self.ontology.items():
+            feature_ids_go = go_genes['genes'].intersection(features_of_interest)
+            background_go = go_genes['genes'].intersection(background)
+            if len(feature_ids_go) <= 3 or len(background_go) < 5:
+                pValues[go_term] = 'notest'
+                continue
+
+            # Survival function is more accurate on small p-values
+            pVal = hypergeom.sf(len(feature_ids_go), n_all_genes,
+                                len(background_go),
+                                n_features_of_interest)
+            if pVal < 0:
+                pVal = 0
+            symbols = []
+            for ensg in feature_ids_go:
+                if ensg in xRef:
+                    symbols.append(xRef[ensg])
+                else:
+                    symbols.append(ensg)
+            pValues[go_term] = (
+                pVal, len(feature_ids_go), len(background_go),
+                len(go_genes['genes']),
+                feature_ids_go,
+                symbols)
+
+        for k, v in pValues.items():
+            try:
+                pValues[k][0] = v * float(n_comparisons)  # bonferroni correction
+            except:
+                pass
+        import operator
+
+        y = []
+
+        sorted_x = sorted(pValues.iteritems(), key=operator.itemgetter(1))
+
+        for k, v in sorted_x:
+            if v == "notest":
+                continue
+            if not type(k) == str:
+                continue
+            try:
+                if v[0] > p_value_cutoff:
+                    continue
+                if if_print:
+                    print k, "|".join(ontology[k]['name']), "%.3e" % v[0], v[
+                        1], \
+                        v[2], v[3], "|".join(v[3])
+                    pass
+                y.append(
+                    [k, "|".join(ontology[k]['name']), v[0], v[1], v[2], v[3],
+                     ",".join(v[4]), ",".join(v[5])])
+
+            except:
+                pass
+        columns = ['go_term_id', 'go_term_description',
+                   'bonferroni_corrected_hypergeometric_'
+                   'p_value',
+                   'n_genes_in_list_and_go_category',
+                   'n_expressed_genes_in_go_category',
+                   'n_genes in_go_category',
+                   'ensembl_gene_ids_in_list',
+                   'gene_symbols_in_ist']
+
+        try:
+            df = pd.DataFrame(y, columns=columns)
+            df.set_index('GO Term ID', inplace=True)
+        except:
+            df = pd.DataFrame(None, columns=columns)
+
+        return df
+
+    def plot_enrichment(self, feature_ids, background, ):
--- flotilla/go.py
+++ flotilla/go.py
@@ -3,109 +3,11 @@ from __future__ import division
 
 from collections import defaultdict
 import gzip
-import itertools
 
 import numpy as np
 import pandas as pd
-from scipy.stats import hypergeom
 
 from flotilla.util import link_to_list
-
-
-def generateOntology(df):
-
-    ontology = defaultdict(
-        lambda: {'genes': set(), 'name': set(), 'domain': set()})
-    allGenesInOntologies = set(df.get('Ensembl Gene ID'))
-    for GO, gene, domain, name in itertools.izip(df.get('GO Term Accession'),
-                                                 df.get('Ensembl Gene ID'),
-                                                 df.get('GO domain'),
-                                                 df.get('GO Term Name')):
-        ontology[GO]['genes'].add(gene)
-        ontology[GO]['name'].add(name)
-        ontology[GO]['domain'].add(domain)
-        ontology[GO]['n_genes'] = len(ontology[GO]['genes'])
-    return ontology, allGenesInOntologies
-
-
-def GO_enrichment(geneList, ontology, expressedGenes=None, printIt=False,
-                  pCut=1000000, xRef={}):
-    lenAllGenes, lenTheseGenes = len(expressedGenes), len(geneList)
-    pValues = defaultdict()
-    nCmps = 0
-
-    for GOTerm, GOGenes in ontology.items():
-        inBoth = GOGenes['genes'].intersection(geneList)
-        expressedGOGenes = GOGenes['genes'].intersection(expressedGenes)
-        if len(inBoth) <= 3 or len(expressedGOGenes) < 5:
-            pValues[GOTerm] = 'notest'
-            continue
-        # survival function is more accurate on small p-values...
-        pVal = hypergeom.sf(len(inBoth), lenAllGenes, len(expressedGOGenes),
-                            lenTheseGenes)
-        if pVal < 0:
-            pVal = 0
-        symbols = []
-        for ensg in inBoth:
-            if ensg in xRef:
-                symbols.append(xRef[ensg])
-            else:
-                symbols.append(ensg)
-        pValues[GOTerm] = (
-            pVal, len(inBoth), len(expressedGOGenes), len(GOGenes['genes']),
-            inBoth,
-            symbols)
-
-    for k, v in pValues.items():
-        try:
-            pValues[k][0] = v * float(nCmps)  # bonferroni correction
-        except:
-            pass
-    import operator
-
-    y = []
-
-    sorted_x = sorted(pValues.iteritems(), key=operator.itemgetter(1))
-
-    for k, v in sorted_x:
-        if v == "notest":
-            continue
-        if not type(k) == str:
-            continue
-        try:
-            if v[0] > pCut:
-                continue
-            if printIt:
-                print k, "|".join(ontology[k]['name']), "%.3e" % v[0], v[1], \
-                    v[2], v[3], "|".join(v[3])
-                pass
-            y.append([k, "|".join(ontology[k]['name']), v[0], v[1], v[2], v[3],
-                      ",".join(v[4]), ",".join(v[5])])
-
-        except:
-            pass
-
-    try:
-        df = pd.DataFrame(y, columns=['GO Term ID', 'GO Term Description',
-                                      'Bonferroni-corrected Hypergeometric '
-                                      'p-Value',
-                                      'N Genes in List and GO Category',
-                                      'N Expressed Genes in GO Category',
-                                      'N Genes in GO category',
-                                      'Ensembl Gene IDs in List',
-                                      'Gene symbols in List'])
-        df.set_index('GO Term ID', inplace=True)
-    except:
-        df = pd.DataFrame(None, columns=['GO Term ID', 'GO Term Description',
-                                         'Bonferroni-corrected Hypergeometric '
-                                         'p-Value',
-                                         'N Genes in List and GO Category',
-                                         'N Expressed Genes in GO Category',
-                                         'N Genes in GO category',
-                                         'Ensembl Gene IDs in List',
-                                         'Gene symbols in List'])
-
-    return df
 
 
 class GO(object):
