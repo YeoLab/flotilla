f6d2ec3f9e33fb9fdb7e176eb1c90b4a225f4967
Olga Botvinnik <olga.botvinnik@gmail.com>
2015-02-26 13:28:16 -0800

    reorganise GeneOntology data, create ontology in __init__, add options for domains, add docstrings

--- flotilla/data_model/gene_ontology.py
+++ flotilla/data_model/gene_ontology.py
@@ -1,6 +1,4 @@
-from collections import defaultdict
-from itertools import izip
-import operator
+from collections import defaultdict, Iterable
 import sys
 
 import pandas as pd
@@ -11,109 +9,195 @@ from ..util import timestamp
 
 class GeneOntologyData(object):
 
-    def __init__(self, data):
+    domains = frozenset(['biological_process', 'molecular_function',
+               'cellular_component'])
 
-        self.data = data
+    def __init__(self, data):
+        """Object to calculate enrichment of Gene Ontology terms
+
+        Acceptable Gene Ontology tables can be downloaded from ENSEMBL's
+        BioMart tool: http://www.ensembl.org/biomart
+
+        1. Choose "Ensembl Genes ##" (## = version number, for me it's 78)
+        2. Click "Attributes"
+        3. Expand "EXTERNAL"
+        4. Check the boxes for 'GO Term Accession', 'Ensembl Gene ID',
+           'GO Term Name', and 'GO domain'
+
+        Parameters
+        ----------
+        data : pandas.DataFrame
+            A dataframe with at least the following columns:
+            'GO Term Accession', 'Ensembl Gene ID', 'GO Term Name', 'GO domain'
+        """
+
+        self.data = data.dropna()
         sys.stdout.write('{}\tBuilding Gene Ontology '
                          'database...\n'.format(timestamp()))
-        ontology, all_genes = self._generate_ontology(self.data)
+        self.ontology = defaultdict(dict)
+        for go, df in data.groupby('GO Term Accession'):
+            self.ontology[go]['genes'] = set(df['Ensembl Gene ID'])
+            self.ontology[go]['name'] = set(df['GO Term Name'])
+            self.ontology[go]['domain'] = set(df['GO domain'])
+            self.ontology[go]['n_genes'] = len(self.ontology[go]['genes'])
         sys.stdout.write('{}\t\tDone.'.format(timestamp()))
-        self.ontology = ontology
-        self.all_genes = all_genes
 
-    def _generate_ontology(self, df):
-
-        ontology = defaultdict(
-            lambda: {'genes': set(), 'name': set(), 'domain': set()})
-        allGenesInOntologies = set(df.get('Ensembl Gene ID'))
-        for GO, gene, domain, name in izip(
-                df.get('GO Term Accession'),
-                df.get('Ensembl Gene ID'),
-                df.get('GO domain'),
-                df.get('GO Term Name')):
-            ontology[GO]['genes'].add(gene)
-            ontology[GO]['name'].add(name)
-            ontology[GO]['domain'].add(domain)
-            ontology[GO]['n_genes'] = len(ontology[GO]['genes'])
-        return ontology, allGenesInOntologies
-
+        self.all_genes = self.data['Ensembl Gene ID'].unique()
 
     def enrichment(self, features_of_interest, background=None,
-                   p_value_cutoff=1000000, xRef={}):
+                   p_value_cutoff=1000000, cross_reference=None,
+                   min_feature_size=3, min_background_size=5,
+                   domain=None):
+        """Bonferroni-corrected hypergeometric p-values of GO enrichment
+
+        Calculates hypergeometric enrichment of the features of interest, in
+        each GO category.
+
+        Parameters
+        ----------
+        features_of_interest : list-like
+            List of features. Must match the identifiers in the ontology
+            database exactly, i.e. if your ontology database is ENSEMBL ids,
+            then you can only provide those and not common names like "RBFOX2"
+        background : list-like, optional
+            Background genes to use. It is best to use a relevant background
+            such as all expressed genes. If None, defaults to all genes.
+        p_value_cutoff : float, optional
+            Maximum accepted Bonferroni-corrected p-value
+        cross_reference : dict-like, optional
+            A mapping of gene ids to gene symbols, e.g. a pandas Series of
+            ENSEMBL genes e.g. ENSG00000139675 to gene symbols e.g HNRNPA1L2
+        min_feature_size : int, optional
+            Minimum number of features of interest overlapping in a GO Term,
+            to calculate enrichment
+        min_background_size : int, optional
+            Minimum number of features in the background overlapping a GO Term
+        domain : str or list, optional
+            Only calculate GO enrichment for a particular GO category or
+            subset of categories. Valid domains:
+            'biological_process', 'molecular_function', 'cellular_component'
+
+        Returns
+        -------
+        enrichment_df : pandas.DataFrame
+            A (n_go_categories, columns) DataFrame of the enrichment scores
+
+        Raises
+        ------
+        ValueError
+            If features of interest and background do not overlap, or invalid
+            GO domains are given
+        """
+        cross_reference = {} if cross_reference is None else cross_reference
         background = self.all_genes if background is None else background
+        if len(set(background) & set(features_of_interest)) == 0:
+            raise ValueError('Features of interest and background do not '
+                             'overlap! Not calculating GO enrichment')
+        domains = self.domains
+
+        if isinstance(domain, str):
+            if domain not in self.domains:
+                raise ValueError("'{}' is not a valid GO domain. Only {} are "
+                                 "acceptable".format(
+                    domain, ",".join("'{}'".format(x) for x in self.domains)))
+            domains = (domain)
+        elif isinstance(domain, Iterable):
+            if len(set(domain) & self.domains) == 0:
+                raise ValueError("'{}' are not a valid GO domains. Only {} are "
+                                 "acceptable".format(
+                    ",".join("'{}'".format(x) for x in domain),
+                    ",".join("'{}'".format(x) for x in self.domains)))
+            domains = domain
+
         n_all_genes = len(background)
         n_features_of_interest = len(features_of_interest)
-        pValues = defaultdict()
-        n_comparisons = 0
+        enrichment = defaultdict(dict)
 
         for go_term, go_genes in self.ontology.items():
-            feature_ids_go = go_genes['genes'].intersection(
+            if go_genes['domain'] not in domains:
+                continue
+
+            features_in_go = go_genes['genes'].intersection(
                 features_of_interest)
-            background_go = go_genes['genes'].intersection(background)
-            if len(feature_ids_go) <= 3 or len(background_go) < 5:
-                pValues[go_term] = 'notest'
+            background_in_go = go_genes['genes'].intersection(background)
+            too_few_features = len(features_in_go) < min_feature_size
+            too_few_background = len(background_in_go) < min_background_size
+            if too_few_features or too_few_background:
                 continue
 
             # Survival function is more accurate on small p-values
-            pVal = hypergeom.sf(len(feature_ids_go), n_all_genes,
-                                len(background_go),
+            p_value = hypergeom.sf(len(features_in_go), n_all_genes,
+                                len(background_in_go),
                                 n_features_of_interest)
-            if pVal < 0:
-                pVal = 0
-            symbols = []
-            for ensg in feature_ids_go:
-                if ensg in xRef:
-                    symbols.append(xRef[ensg])
-                else:
-                    symbols.append(ensg)
-            pValues[go_term] = (
-                pVal, len(feature_ids_go), len(background_go),
-                len(go_genes['genes']),
-                feature_ids_go,
-                symbols)
+            p_value = 0 if p_value < 0 else p_value
+            symbols = [cross_reference[f] if f in cross_reference else f for f
+                       in features_in_go]
+            enrichment['p_value'][go_term] = p_value
+            enrichment['n_features_of_interest_in_go_term'][go_term] = len(
+                features_in_go)
+            enrichment['n_background_in_go_term'][go_term] = len(
+                background_in_go)
+            enrichment['n_features_total_in_go_term'][go_term] = len(
+                go_genes['genes'])
+            enrichment['features_of_interest_in_go_term'][
+                go_term] = ','.join(features_in_go)
+            enrichment['features_of_interest_in_go_term_gene_symbols'][
+                go_term] = ','.join(symbols)
+        enrichment_df = pd.DataFrame(enrichment)
 
-        for k, v in pValues.items():
-            try:
-                # Bonferroni correction
-                pValues[k][0] = v * float(n_comparisons)
-            except TypeError:
-                pass
+        # Bonferonni correction
+        enrichment_df['bonferonni_corrected_p_value'] = \
+            enrichment_df.p_value * enrichment_df.shape[0]
+        ind = enrichment_df['bonferonni_corrected_p_value'] < p_value_cutoff
+        enrichment_df = enrichment_df.ix[ind]
 
-        y = []
+        metadata = self.data.ix[:, ['GO domain', 'GO Term Name',
+                                    'GO Term Accession']]
+        metadata = metadata.set_index('GO Term Accession')
+        enrichment_df = enrichment_df.join(metadata)
+        return enrichment_df
+        # for k, v in pValues.items():
+        #     try:
+        #         # Bonferroni correction
+        #         pValues[k][0] = v * float(n_comparisons)
+        #     except TypeError:
+        #         pass
+        #
+        # y = []
+        #
+        # sorted_x = sorted(pValues.iteritems(),
+        #                   key=operator.itemgetter(1))
+        #
+        # for k, v in sorted_x:
+        #     if v == "notest":
+        #         continue
+        #     if not type(k) == str:
+        #         continue
+        #     try:
+        #         if v[0] > p_value_cutoff:
+        #             continue
+        #         y.append(
+        #             [k, "|".join(self.ontology[k]['name']), v[0], v[1],
+        #              v[2], v[3], ",".join(v[4]), ",".join(v[5])])
+        #
+        #     except:
+        #         pass
+        # columns = ['GO Term ID', 'GO Term Description',
+        #            'Bonferroni-corrected Hypergeometric '
+        #            'p-Value',
+        #            'N Genes in List and GO Category',
+        #            'N Expressed Genes in GO Category',
+        #            'N Genes in GO category',
+        #            'Ensembl Gene IDs in List',
+        #            'Gene symbols in List']
+        #
+        # try:
+        #     df = pd.DataFrame(y, columns=columns)
+        #     df.set_index('Go Term ID', inplace=True)
+        # except:
+        #     df = pd.DataFrame(None, columns=columns)
 
-        sorted_x = sorted(pValues.iteritems(),
-                          key=operator.itemgetter(1))
-
-        for k, v in sorted_x:
-            if v == "notest":
-                continue
-            if not type(k) == str:
-                continue
-            try:
-                if v[0] > p_value_cutoff:
-                    continue
-                y.append(
-                    [k, "|".join(self.ontology[k]['name']), v[0], v[1],
-                     v[2], v[3], ",".join(v[4]), ",".join(v[5])])
-
-            except:
-                pass
-        columns = ['GO Term ID', 'GO Term Description',
-                   'Bonferroni-corrected Hypergeometric '
-                   'p-Value',
-                   'N Genes in List and GO Category',
-                   'N Expressed Genes in GO Category',
-                   'N Genes in GO category',
-                   'Ensembl Gene IDs in List',
-                   'Gene symbols in List']
-
-        try:
-            df = pd.DataFrame(y, columns=columns)
-            df.set_index('Go Term ID', inplace=True)
-        except:
-            df = pd.DataFrame(None, columns=columns)
-
-        return df
+        # return df
 
     def plot_enrichment(self, feature_ids, background, ):
         pass
