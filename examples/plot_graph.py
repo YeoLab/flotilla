5e915a970118a96b28e65ed02c113a2570dde8f5
Olga Botvinnik <olga.botvinnik@gmail.com>
2015-04-10 15:39:01 -0400

    use brainspan instead of shalek2013 data as examples

--- examples/plot_graph.py
+++ examples/plot_graph.py
@@ -7,5 +7,5 @@ See also
 
 """
 import flotilla
-study = flotilla.embark(flotilla._shalek2013)
-study.plot_graph(cov_std_cut=0.5)
+study = flotilla.embark(flotilla._brainspan)
+study.plot_graph(cov_std_cut=3, degree_cut=5)
