a9c0a8a0bfe0a996d416a9695000afb5f6ab005e
Jessica Lettes <jlettes@ucsd.edu>
2015-04-30 10:33:19 -0700

    cast map and zip objects to list

--- flotilla/test/data_model/test_metadata.py
+++ flotilla/test/data_model/test_metadata.py
@@ -78,14 +78,14 @@ class TestMetaData(object):
         else:
             true_phenotype_to_marker = phenotype_to_marker
 
-        true_phenotype_transitions = zip(true_phenotype_order[:-1],
-                                         true_phenotype_order[1:])
+        true_phenotype_transitions = list(zip(true_phenotype_order[:-1],
+                                         true_phenotype_order[1:]))
         true_unique_phenotypes = self.metadata[self.phenotype_col].unique()
         true_n_phenotypes = len(true_unique_phenotypes)
 
-        true_colors = map(mpl.colors.rgb2hex,
+        true_colors = list(map(mpl.colors.rgb2hex,
                           sns.color_palette('husl',
-                                            n_colors=true_n_phenotypes))
+                                            n_colors=true_n_phenotypes)))
         colors = iter(true_colors)
         true_default_phenotype_to_color = defaultdict(lambda: colors.next())
 
