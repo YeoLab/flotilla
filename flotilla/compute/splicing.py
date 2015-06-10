a6e1f52a5a70cbfc7fe12e8482e8e252c7b7605b
Olga Botvinnik <olga.botvinnik@gmail.com>
2015-06-10 14:33:45 -0700

    modality_assignements is a series

--- flotilla/compute/splicing.py
+++ flotilla/compute/splicing.py
@@ -178,8 +178,7 @@ class ModalityEstimator(object):
             modality_assignments2.values
 
         # Add back the NA columns
-        modality_assignments = modality_assignments.reindex(
-            columns=data.columns)
+        modality_assignments = modality_assignments.reindex(data.columns)
         return modality_assignments
 
 
