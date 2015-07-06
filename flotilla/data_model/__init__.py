fb416303a2abad5ef51e988de1f4dd460d0dac71
Olga Botvinnik <olga.botvinnik@gmail.com>
2015-07-06 10:30:23 -0700

    remove SpikeInData, move study.py out of DataModel
    Former-commit-id: 69ab2ed3da1f6e37a5ecfd5a5562786d42c65193

--- flotilla/data_model/__init__.py
+++ flotilla/data_model/__init__.py
@@ -1,11 +1,10 @@
-from .expression import ExpressionData, SpikeInData
+from .expression import ExpressionData
 from .gene_ontology import GeneOntologyData
 from .metadata import MetaData
 from .quality_control import MappingStatsData
 from .splicing import SplicingData
-from .study import Study
 
 __author__ = 'olga'
 
-__all__ = ['Study', 'ExpressionData', 'SpikeInData', 'GeneOntologyData',
+__all__ = ['ExpressionData', 'GeneOntologyData',
            'MetaData', 'MappingStatsData', 'SplicingData']
