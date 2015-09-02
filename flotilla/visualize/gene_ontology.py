be649bf25d370817bd03fcd891938ed34ff8dd7c
Olga Botvinnik <olga.botvinnik@gmail.com>
2015-09-02 09:09:01 -0700

    Use Machine epsilon for p=0, and use the FIRST max_categories, not the last (thought data was sorted the other way..)

--- flotilla/visualize/gene_ontology.py
+++ flotilla/visualize/gene_ontology.py
@@ -2,27 +2,29 @@ import matplotlib.pyplot as plt
 import numpy as np
 import seaborn as sns
 
-def plot_go_enrichment(go, ax=None):
-    go.bonferonni_corrected_p_value = go.bonferonni_corrected_p_value.replace(0, np.nan)
-    vmin = max(go.bonferonni_corrected_p_value.dropna().min(), 1e-25)
+
+MACHINE_EPSILON = np.finfo(float).eps
+
+def plot_go_enrichment(x_col='bonferonni_corrected_p_value', data=None,
+                       ax=None, max_categories=10,
+                       **kwargs):
+    data = data.sort(subset=[x_col], ascending=True)
+    data[x_col] = data[x_col].replace(0, np.nan)
+    vmin = max(data[x_col].dropna().min(), MACHINE_EPSILON)
     if np.isnan(vmin):
         vmin = 1e-25
-    go.loc[:, 'bonferonni_corrected_p_value'] = go.bonferonni_corrected_p_value.fillna(vmin*.9)
-    if go.shape[0] > 20:
-        go = go.iloc[-20: , :]
-    go_subset = go
+    data.loc[:, x_col] = data[x_col].fillna(vmin * .9)
+    if data.shape[0] > max_categories:
+        data = data.iloc[max_categories:, :]
     if ax is None:
-        fig, ax = plt.subplots(figsize=(12, max(go_subset.shape[0]*.25, 4)))
-    else:
         ax = plt.gca()
-        fig = plt.gcf()
 
-    bottom = np.arange(go_subset.shape[0])
-    width = -np.log10(go_subset.bonferonni_corrected_p_value)
-    ax.barh(bottom, width)
+    bottom = np.arange(data.shape[0])
+    width = -np.log10(data[x_col])
+    ax.barh(bottom, width, **kwargs)
     xticks = list(sorted(list(set(int(x) for x in ax.get_xticks()))))
-    ax.set(yticks=bottom+0.4, yticklabels=go_subset.go_name, xlabel='$-\log_{10} p$-value',
-           ylim=(0, bottom.max()+1))
+    ax.set(yticks=bottom + 0.4, yticklabels=data.go_name, xlabel='$-\log_{10}(q)$',
+           ylim=(0, bottom.max() + 1))
     sns.despine()
-    fig.tight_layout()
+    #     fig.tight_layout()
     return ax
