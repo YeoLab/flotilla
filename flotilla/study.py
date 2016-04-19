93a89d52e7555fdca2cca313c4bb065d661b9820
Olga Botvinnik <olga.botvinnik@gmail.com>
2016-04-19 15:40:54 -0700

    Remove interactive outliers from study object

--- flotilla/study.py
+++ flotilla/study.py
@@ -1677,7 +1677,5 @@ class Study(object):
 Study.interactive_classifier = Interactive.interactive_classifier
 Study.interactive_graph = Interactive.interactive_graph
 Study.interactive_pca = Interactive.interactive_pca
-Study.interactive_choose_outliers = Interactive.interactive_choose_outliers
-Study.interactive_reset_outliers = Interactive.interactive_reset_outliers
 Study.interactive_clustermap = Interactive.interactive_clustermap
 Study.interactive_correlations = Interactive.interactive_correlations
